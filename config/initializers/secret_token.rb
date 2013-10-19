c6ce4f1cacaa80c80d806407df1e88ade89b72e6
Hugo Amorim <hugoamorimlyra@gmail.com>
2013-10-18 21:06:59 -0300

    Start Rumble 2013 Sunshines :star

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartrooms::Application.config.secret_key_base = '3f99f417df6e625a81da9cd5c7aeeddeebde1e1d68331a76d78ba8fcfa46edfaef9bfcd972a39b3182c5a3686516dc852fb04d98c866d56763042be4f0536020'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smartrooms::Application.config.session_store :cookie_store, key: '_smartrooms_session'
