d9a4261c2ced5bc75da4c8152b9073d295351706
Hugo Amorim <hugoamorimlyra@gmail.com>
2013-10-18 22:48:36 -0300

    Add some :balloon: on the views.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019010802) do
+ActiveRecord::Schema.define(version: 20131019013904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: true do |t|
+    t.datetime "initial_date"
+    t.datetime "end_date"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rooms", force: true do |t|
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
