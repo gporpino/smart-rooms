ef2ea0dd20ae277af7d4d73e81e7449fb5403422
Gustavo Porpino <gporpino@gmail.com>
2013-10-20 16:49:26 -0300

    add start to search

--- app/controllers/smart_reservations_controller.rb
+++ app/controllers/smart_reservations_controller.rb
@@ -1,3 +1,4 @@
+require 'time'
 class SmartReservationsController < ApplicationController
 	skip_authorization_check
 
@@ -18,7 +19,7 @@ class SmartReservationsController < ApplicationController
   def facets
 
 
-  	@result = ['room', 'date', 'length']
+  	@result = ['starts', 'date', 'length']
 
   	render json: @result
   end
@@ -28,16 +29,29 @@ class SmartReservationsController < ApplicationController
 
   	case params.require(:facet)
     	when 'room' then
-    		result = Array.new
-        Room.all.map  |r|	 { 
-        	result << { value: name, label: name }]
-        }
+    		@result = Room.all.map do |r|
+  			
+  				{value: r.name, label: r.name, id: r.id}
+
+  			end
 
       when 'date' then
         @result = ["today","tomorrow","day after tomorrow","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","in 1 days","in 2 days","in 3 days","in 4 days","in 5 days","in 6 days","next Monday","next Tuesday","next Wednesday","next Thursday","next Friday","next Saturday","next Sunday"]
 
       when 'length' then
         @result = ["0.5 hours","1 hour","1.5 hours","2 hours","2.5 hours","3 hours","3.5 hours","4 hours","4.5 hours","5 hours","5.5 hours","6 hours","6.5 hours","7 hours","7.5 hours","8 hours","8.5 hours"]
+
+
+      when 'starts' then
+
+      	time_start = DateTime.now.midnight
+      	time_end = (time_start + 1.day).midnight - 15.minutes
+
+      	@result = [time_start].tap { |array| array << array.last + 15.minutes while array.last < time_end }
+
+    		@result = @result.map do |r|
+					r.to_s(format = :time)
+				end
 
     end
 
--- app/helpers/smart_reservations_helper.rb
+++ app/helpers/smart_reservations_helper.rb
@@ -1,2 +1,7 @@
 module SmartReservationsHelper
+	class Time
+	  def to(to, step = 15.minutes)
+	    [self].tap { |array| array << array.last + step while array.last < to }
+	  end
+	end
 end
