3f863b2b839795cd4e9ed9c7013b922db3faac23
Gustavo Porpino <gporpino@gmail.com>
2013-10-20 02:27:45 -0300

    creating validation of initial date smmaller than now and end date smaller or equal than initial

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,6 +8,8 @@ class Reservation < ActiveRecord::Base
   validates :room_id, presence: true
 
   validate :interception
+  validate :initial_date_cannot_be_in_the_past
+  validate :end_date_grather_than_initial_date
 
   default_scope { order("initial_date ASC") }
 
@@ -18,8 +20,18 @@ class Reservation < ActiveRecord::Base
      end_date: end_date, initial_date: initial_date).count
 
     if count > 0
-      errors.add(:base, 'This reservation has date time insterceped with other reservation.')
+      errors.add(:base, 'This reservation has intercession date / time with another reservation')
     end
+  end
+
+  def initial_date_cannot_be_in_the_past
+    errors.add(:initial_date, "can't be in the past") if
+      !initial_date.blank? and initial_date < Date.today
+  end
+
+  def end_date_grather_than_initial_date
+    errors.add(:end_date, "must be grather than initial date") if
+      end_date <= initial_date 
   end
 
 end
