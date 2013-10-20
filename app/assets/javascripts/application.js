eaaca39df403cce6664e07dbddcc75f86aa7159f
Hugo Amorim <hugoamorimlyra@gmail.com>
2013-10-20 20:49:41 -0300

    Adjust things.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,81 @@
 //= require jquery.dp_calendar.min
 //= require visualsearch
 //= require_tree .
+
+
+String.prototype.format = String.prototype.f = function () {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
+String.prototype.replaceAll = function (find, replace) {
+    return this.replace(new RegExp(find, 'g'), replace);
+};
+
+$(document).ready(function () {
+    var visualSearch = VS.init({
+        container: $('.visual_search'),
+        query: '',
+        callbacks: {
+            search: function (query, searchCollection) {
+
+                json = {};
+                if (query.trim() != "") {
+                    queries = query.split('" ');
+
+                    for (i = 0; i < queries.length; i = i + 1) {
+                        paramText = queries[i].replaceAll('"', '');
+
+                        params = paramText.split(": ");
+
+                        j = $.parseJSON('{ "{0}":"{1}" }'.f(params[0], params[1]));
+                        $.extend(json, j);
+                    }
+                }
+
+                $.get("/smart_reservations/search", json, function (result) {
+                    $('#result').html(result);
+                });
+
+            },
+            facetMatches: function (callback) {
+
+                query = visualSearch.searchBox.currentQuery;
+
+                json = {};
+                if (query.trim() != "") {
+                    queries = query.split('" ');
+
+                    for (i = 0; i < queries.length; i = i + 1) {
+                        paramText = queries[i].replaceAll('"', '');
+
+                        params = paramText.split(": ");
+
+                        j = $.parseJSON('{ "{0}":"{1}" }'.f(params[0], params[1]));
+                        $.extend(json, j);
+                    }
+                }
+
+                $.getJSON("/smart_reservations/facets.json", json, function (data) {
+                    callback(data)
+                });
+            },
+            valueMatches: function (facet, searchTerm, callback) {
+                $.getJSON("/smart_reservations/values/" + facet + ".json", function (data) {
+                    callback(data)
+                });
+
+            }
+        }
+    });
+});
+
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
+
