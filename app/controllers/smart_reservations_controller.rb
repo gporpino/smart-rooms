96121736712736ad29af21b014544e78a4b0172c
Gustavo Porpino <gporpino@gmail.com>
2013-10-20 14:30:30 -0300

    partial solution of smart filed

---
+++ app/controllers/smart_reservations_controller.rb
@@ -0,0 +1,36 @@
+class SmartReservationsController < ApplicationController
+	skip_authorization_check 
+
+	
+	
+	def index
+    
+  end
+
+  def facets
+
+  	@result = ['room', 'when', 'time']
+  	debugger
+  	@result
+  end	
+
+  def value_matches facet
+
+  	case facet
+    	when 'room' then
+        @result = [ 	{ value: '1', label: 'ITBC' },
+          					{ value: '2', label: 'Cultura' },
+          					{ value: '3', label: 'The Hub' } ]
+      
+      when 'when' then
+        @result = ['tomorrow', 'today']
+
+      
+      when 'time' then
+        @result = ['10:00', 'hugoamorimlyra@gmail.com', 'lucianoncoutinho@hotmail.com']
+    end
+
+    @result
+  end
+
+end
---
+++ app/helpers/smart_reservations_helper.rb
@@ -0,0 +1,2 @@
+module SmartReservationsHelper
+end
